2bec1653f697e02d52b6a2004bdc89e721e5f3b4
ahkalama <116187665+ahkalama@users.noreply.github.com>
2024-12-07 19:32:53 +0300

    Update facetracking.py

--- pythondenemeler/facetracking.py
+++ pythondenemeler/facetracking.py
@@ -7,24 +7,16 @@ face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_fronta
 
 try:
     while True:
-
         screenshot = pyautogui.screenshot()
-
         frame = np.array(screenshot)
-
         frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
         gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
         faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
 
         for (x, y, w, h) in faces:
-
             cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
             face_center_x = x + w // 2
             face_center_y = y + h // 2
-
             pyautogui.moveTo(face_center_x, face_center_y)
 
         cv2.imshow('Masaustu Yuz Takibi', frame)
